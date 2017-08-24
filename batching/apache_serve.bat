75acab66c2369e4cd8f413905863ae160185b7fc
Ragavan <ragavanpv@revature.com>
2017-08-24 14:19:55 +0530

    batch script improvements and added a call for script file from java

--- batching/apache_serve.bat
+++ batching/apache_serve.bat
@@ -1,11 +1,37 @@
 @echo off
-httpd -version > nul
-if errorlevel 0 (
-   echo Apache has already been installed
-) else (
-   echo Installing Apache Server...
-   scoop install apache
+if "%1"==install (
+  call :Install
+  EXIT /B %errorlevel%
+)
+if "%1"==start (
+  call :Start
+  EXIT /B %errorlevel%
+)
+if "%1"==stop (
+  call :Stop
+  EXIT /B %errorlevel%
+)
+if "%1"=="" (
+  echo incorrect usage
+  EXIT /B %errorlevel%
 )
 
-httpd" -k start
-httpd" -k stop
+:Start
+echo Starting Apache server
+httpd -k start
+EXIT /B 0
+
+:Stop
+echo Stoping Apache server
+httpd -k stop
+EXIT /B 0
+
+:Install
+httpd -version > nul
+if errorlevel 9009 (
+  echo Installing Apache Server...
+  scoop install apache
+) else (
+  echo Apache has already been installed
+)
+EXIT /B 0
--- gradling/src/main/com/aynna/gradling/Main.java
+++ gradling/src/main/com/aynna/gradling/Main.java
@@ -1,12 +1,25 @@
 package com.aynna.gradling;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
 
   public static void main(String[] args) {
+      int buf =0;
+      StringBuffer stringBuffer = new StringBuffer();
+      String command = "/C:\\Users\\Revature\\Documents\\Vighnesh\\devops\\batching\\apache_serve.bat";
 
-    System.out.println(Arrays.toString(args));
+    try {
+      Process process = Runtime.getRuntime().exec(command);
+      InputStream inputStream = process.getInputStream();
+      while ((buf=inputStream.read()) != -1){
+          stringBuffer.append((char)buf);
+      }
+        System.out.println("Response: " + stringBuffer.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
   }
 
