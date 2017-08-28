4d8f3bedfc4695af4ddd7e4f2774cde60f160ac0
jeevavighnesh <jeevavignesh@gmail.com>
2017-08-28 08:34:58 +0530

    Changed flow of batch script added a path to batch file in java project bug fix scripts

--- gradling/src/main/com/aynna/gradling/Main.java
+++ gradling/src/main/com/aynna/gradling/Main.java
@@ -1,27 +1,33 @@
 package com.aynna.gradling;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main {
 
   public static void main(String[] args) {
-      int buf =0;
+      String buf = "";
       StringBuffer stringBuffer = new StringBuffer();
-      final String flag = "s";
-      String command = "/home/vighnesh/Documents/devops/scripting/serve/tomcat_server.sh -" + flag;
-//      String command = "/home/vighnesh/Documents/devops/scripting/serve/apache_serve.sh -" + flag;
-//      String command = "//Documents/devops/scripting/serve/tomcat_server.sh";
+      final String flag = "i";
+      String username = "vikki";
+//      String command = "bash /home/" + username + "/Documents/devops/scripting/serve/tomcat_server.sh -" + flag;
+      String command = "sudo bash /home/" + username + "/Documents/devops/scripting/serve/apache_serve.sh -" + flag;
+//      String command = "C:\\Users\\Admin\\Documents\\WindowsPowerShell\\devops\\batching\\apache_serve.bat start";
 
     try {
       Process process = Runtime.getRuntime().exec(command);
-      InputStream inputStream = process.getInputStream();
-      while ((buf=inputStream.read()) != -1){
-          stringBuffer.append((char)buf);
+      process.waitFor();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      while ((buf=reader.readLine()) != null){
+          stringBuffer.append(buf + "\n");
       }
         System.out.println("Response: " + stringBuffer.toString());
     } catch (IOException e) {
-      e.printStackTrace();
+        e.printStackTrace();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
     }
 
   }
