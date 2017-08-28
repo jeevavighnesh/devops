da605e4bcff6ba8a10c4e42aeedcb67a7c1c4399
Ragavan <ragavanpv@revature.com>
2017-08-28 19:11:33 +0530

    Bug fix batch

--- batching/apache_serve.bat
+++ batching/apache_serve.bat
@@ -1,37 +1,43 @@
 @echo off
-if "%1"==install (
+if "%1"=="install" (
   call :Install
   EXIT /B %errorlevel%
 )
-if "%1"==start (
+if "%1"=="start" (
   call :Start
   EXIT /B %errorlevel%
 )
-if "%1"==stop (
+if "%1"=="stop" (
   call :Stop
   EXIT /B %errorlevel%
 )
-if "%1"=="" (
+if "%1"=="uninstall" (
+  call :Uninstall
+  EXIT /B %errorlevel%
+) else (
   echo incorrect usage
   EXIT /B %errorlevel%
 )
 
+:Uninstall
+echo Terminating apache
+net stop apache
+echo Removing apache server
+choco uninstall apache-httpd -y
+echo Successfully removed apache server
+EXIT /b 0
+
 :Start
 echo Starting Apache server
-httpd -k start
+net start apache
 EXIT /B 0
 
 :Stop
 echo Stoping Apache server
-httpd -k stop
+net stop apache
 EXIT /B 0
 
 :Install
-httpd -version > nul
-if errorlevel 9009 (
-  echo Installing Apache Server...
-  scoop install apache
-) else (
-  echo Apache has already been installed
-)
+echo Installing Apache Server...
+choco install apache-httpd -y
 EXIT /B 0
